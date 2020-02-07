baa3691a2787bafd71be2fcee3c88d58281db906
Swaleh Hussein <swaleh.hussein@vault.wananchi.com>
2020-02-07 11:50:21 +0300

    Firebase email & password auth fixed

---
+++ app/src/main/java/com/example/pick/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.pick;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.email_edit_text)
+    EditText email_edit_text;
+    @BindView(R.id.password_edit_text)
+    EditText password_edit_text;
+    @BindView(R.id.sign_up_button)
+    Button sign_up_button;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount();
+            }
+        });
+    }
+
+    private void createAccount() {
+        String email = email_edit_text.getText().toString();
+        String password = password_edit_text.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Sign up sucessful", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Sign up unsucessful", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+}
